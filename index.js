1c6d2f05122e11e077f0a4665ff70df4d186d749
Leonel Lopez Pisani <leolopezpisani@gmail.com>
2024-04-28 20:25:36 -0300

    project resolution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const upload = multer();
+require('dotenv').config();
 
 var app = express();
 
@@ -11,7 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  var file = req.file;
 
+  var obj = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+
+  res.json(obj);
+});
 
 
 const port = process.env.PORT || 3000;
